79543d3b12f3b899997de0ed0b3795207d168dd5
Timur Karimov <timurr.mov@gmail.com>
2024-03-16 01:50:19 +0500

    ibs2

--- mock_kafka_ibs.py
+++ mock_kafka_ibs.py
@@ -1,27 +1,27 @@
 from flask import Flask, request, jsonify
-from confluent_kafka import Producer
+from kafka import KafkaProducer
 import json
 
-
 app = Flask(__name__)
-producer = Producer({'bootstrap.servers': 'localhost:9092'})
+app.debug = True
+
+# Создаем Kafka producer
+producer = KafkaProducer(bootstrap_servers='localhost:9092',
+                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 
-def process_data(data):
+@app.route('/test', methods=['POST'])
+def process_data():
+    data = request.get_json()
+    # Обрабатываем данные (меняем возраст на 96)
     data['age'] = 96
-    return data
 
 
-@app.route('/process_data', methods=['POST'])
-def process_rest_request():
-    req_data = request.get_json()
-    processed_data = process_data(req_data)
+    # Отправляем обработанные данные в Kafka
+    producer.send('test', value=data)
+    print(data)
 
-    # Отправка в kafka
-    producer.produce('processed_data_topic', value=bytes(json.dumps(processed_data), encoding='utf-8'))
-    producer.flush()
-
-    return jsonify({'message': 'Data processed and sent to Kafka'})
+    return jsonify({"message": "Data processed and sent to Kafka"})
 
 
 if __name__ == '__main__':
